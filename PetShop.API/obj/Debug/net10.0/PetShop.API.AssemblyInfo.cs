b55e7fef2db2c18294d40fd39ed8c8aca3e66b6f
lunaguima <luna.dcarvalho05@gmail.com>
2026-03-14 17:46:16 -0300

    feat: adicionando classes Appointment e OrderItem

--- PetShop.API/obj/Debug/net10.0/PetShop.API.AssemblyInfo.cs
+++ PetShop.API/obj/Debug/net10.0/PetShop.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af60e1520638c64a47badbc0e6d8862a1cb14fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ PetShop.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace PetShop.Domain.Enums;
+
+public enum StatusEnum
+{
+    Scheduled,    // Agendado
+    InProgress,  // Em andamento
+    Completed,  // Concluído
+    Cancelled  // Cancelado
+}
