5ce9a9f300481ff73d934c0cef34434e5c1f172c
sSofia-s <ssofiasiq@gmail.com>
2026-03-15 16:10:25 -0300

    feat(Order): criado entidade Order

---
+++ src/PetShop.Domain/Entities/Order.cs
@@ -0,0 +1,28 @@
+﻿using PetShop.Domain.Commom;
+
+namespace PetShop.Domain.Entities;
+
+public class Order : BaseEntity
+{
+    public Guid OrderId { get; private set; } 
+    
+    public DateTime OrderDate  { get; private set; }
+    
+    public decimal TotalAmount  { get; private set; }
+    
+    public Guid CustomerId { get; private set; }
+    
+    // public Costumer Costumer { get; private set; }
+    
+    
+    // Relacionamento N:1 e 1:N
+    // Order N:1 Customer
+    // Order 1:N OrderItem
+
+    public Order(Guid orderId, DateTime orderDate, decimal totalAmount)
+    {
+        OrderId = orderId;
+        OrderDate = orderDate;
+        TotalAmount = totalAmount;
+    }
+}
