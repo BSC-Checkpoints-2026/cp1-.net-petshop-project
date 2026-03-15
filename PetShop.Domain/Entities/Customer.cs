c176711f78eeb3d83617d2f47dd2ec5fd5a983bf
Moisés Barsoti <moises.barsot2@gmail.com>
2026-03-14 21:27:50 -0300

    feat(Customer): adicionado construtor com passagem de parâmetros

--- PetShop.Domain/Entities/Customer.cs
+++ PetShop.Domain/Entities/Customer.cs
@@ -8,7 +8,7 @@ public class Customer : BaseEntity
 
     public string LastName { get; private set; }
 
-    public string CPF { get; private set; }
+    public string Cpf { get; private set; }
 
     public string Email { get; private set; }
 
@@ -17,6 +17,16 @@ public class Customer : BaseEntity
     public string Address { get; private set; }
     
     // Relacionamentos 
-    // public List<Pet> Pets { get; set; } = new List<Pet>();
+    public List<Pet> Pets { get; set; } = new List<Pet>();
     // public List<Order> Orders { get; set; } = new List<Order>();
+    
+    public Customer(string firstName, string lastName, string cpf, string email, string phoneNumber, string address) 
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Cpf = cpf;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+    }
 }
