dd279cea709373a9cc89d95c2c8b389476f104ef
Moisés Barsoti <moises.barsot2@gmail.com>
2026-03-14 22:41:00 -0300

    fix: resolvendo conflitos de merge'

--- PetShop.Domain/obj/Debug/net10.0/PetShop.Domain.AssemblyInfo.cs
+++ PetShop.Domain/obj/Debug/net10.0/PetShop.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af60e1520638c64a47badbc0e6d8862a1cb14fb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8707bc1513710aea626d8c8e24325dcb1e3756bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
